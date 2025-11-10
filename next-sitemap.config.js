c2438cdbbd31473ea1bef51ddb097cfc7dd5ef24
vercelblog <vercelblog@gmail.com>
2025-11-10 13:07:10 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl:'https://wepzite.in',
+  siteUrl:'https://javabro.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
@@ -8,7 +8,7 @@ module.exports = {
   exclude: ['/admin/*', '/api/*'],
   robotsTxtOptions: {
     additionalSitemaps: [
-      `https://wepzite.in/sitemap.xml`,
+      `https://javabro.com/sitemap.xml`,
     ],
   },
 };
